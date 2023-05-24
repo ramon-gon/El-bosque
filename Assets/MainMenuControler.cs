f739870e672fb9ce652936a7d83c1ac0433e40b7
=Ramon Gonzalez <=gonzalez.guix.ramon@alumnat.copernic.cat>
2023-05-24 15:29:01 +0200

    menu

---
+++ Assets/MainMenuControler.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuControler : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        
+    }
+
+    public void OpenOptions()
+    {
+        // Lógica para abrir la pantalla de opciones
+    }
+
+    public void QuitGame()
+    {
+
+        Application.Quit();
+    }
+}
---
+++ Assets/quit.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Quit : MonoBehaviour
+{
+public GameObject  Menu_principal;
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
